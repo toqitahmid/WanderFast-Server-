fec519c25a29dff66a331705f77c9b1f5af5cd23
toqitahmid <rubaiathali@gmail.com>
2026-06-28 19:40:48 +0600

    set up the server using express js

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+const uri = process.env.MONGODB_URI;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } 
+  catch(err){
+    console.log(err);
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res) => {
+    res.send('Server is running fine!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
+
+// uri = mongodb+srv://wander_fast:4QBEfe7swRteL1sR@cluster0.mltnagh.mongodb.net/?appName=Cluster0
